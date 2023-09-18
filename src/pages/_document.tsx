869b2c80c04dc4a33b3ec310de02e2bdbf7b68ab
ballsona0925@naver.com <ballsona0925@naver.com>
2023-09-19 02:00:15 +0900

    fix: fix document errors

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
-const Document = () => {
+const Document = () => (
   <Html lang="en">
     <Head />
     <body>
       <Main />
       <NextScript />
     </body>
-  </Html>;
-};
-
+  </Html>
+);
 export default Document;
