989e80478f9a3d74972761e844a37da1921509ab
ballsona0925@naver.com <ballsona0925@naver.com>
2023-09-19 02:01:05 +0900

    feat: add main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import NavigationBar from '@/components/common/NavigationBar';
+import MainBanner from '@/components/main/MainBanner';
+import ArtContent from '@/components/main/ArtContent/ArtContent';
 
 const Home = () => (
   <>
@@ -8,7 +11,10 @@ const Home = () => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <div>Objet Frontend</div>
+
+    <NavigationBar />
+    <MainBanner />
+    <ArtContent />
   </>
 );
 
