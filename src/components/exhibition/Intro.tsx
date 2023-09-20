7f0916d18e214064823c23302d6cb1d40d9451f0
JjungminLee <julie0964@naver.com>
2023-09-20 10:11:44 +0900

    feat:전시조회 퍼블리싱

--- src/components/exhibition/Intro.tsx
+++ src/components/exhibition/Intro.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import { COLORS } from '@/constants/styles';
 import ArtistInfo, { ArtistInfoProps } from '../common/ArtistInfo';
 import Text from '../common/Text';
@@ -10,21 +11,30 @@ export interface IntroProps {
   artistInfo: ArtistInfoProps;
 }
 
-const Intro = () => (
-  <styles.IntroBackground>
-    <Text textStyleName="title" color={COLORS.main.white}>
-      캐시로지
-    </Text>
-    <Text textStyleName="subtitle" color={COLORS.bg.opa60White}>
-      작품설명쓰
-    </Text>
-    <DoorIcon />
-    <ArtistInfo
-      img="https://images.unsplash.com/photo-1694572620159-e69049aeba00?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2970&q=80"
-      part="UXUI디자이너"
-      info="나는 유어슈 미녀 로지"
-      name="로지"
-    />
-  </styles.IntroBackground>
-);
+const Intro = () => {
+  const router = useRouter();
+  const onClick = () => {
+    router.push('/exhibition');
+  };
+  return (
+    <styles.IntroBackground>
+      <Text textStyleName="title" color={COLORS.main.white}>
+        캐시로지
+      </Text>
+      <Text textStyleName="subtitle" color={COLORS.bg.opa60White}>
+        작품설명쓰
+      </Text>
+      <div onClick={onClick}>
+        <DoorIcon />
+      </div>
+      <ArtistInfo
+        img="https://images.unsplash.com/photo-1694572620159-e69049aeba00?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2970&q=80"
+        part="UXUI디자이너"
+        info="나는 유어슈 미녀 로지"
+        name="로지"
+      />
+    </styles.IntroBackground>
+  );
+};
+
 export default Intro;
--- src/components/exhibition/index.tsx
+++ src/components/exhibition/index.tsx
@@ -1,4 +1,42 @@
-import Intro from './Intro';
+import ArtistInfo from '../common/ArtistInfo';
+import * as styles from './Exhibition.style';
+import ExhibitionItem, { ExhibitionItemProps } from './ExhibitionItem';
+import TopBar from './TopBar';
 
-const Exhibition = () => <Intro />;
+export interface ExhibitionProps {
+  title: string;
+  artistName: string;
+  part: string;
+  artistInfo: string;
+  exhibitionItem: ExhibitionItemProps[];
+}
+
+const Exhibition = ({
+  title,
+  part,
+  artistName,
+  artistInfo,
+  exhibitionItem,
+}: ExhibitionProps) => (
+  <styles.ExhibitionBackground>
+    <styles.TopBarContainer>
+      <TopBar
+        imgUrl="https://images.unsplash.com/photo-1694900565922-d279cef76fd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3087&q=80"
+        artistName={artistName}
+        title={title}
+      />
+    </styles.TopBarContainer>
+    {exhibitionItem.map((item) => (
+      <div key={item.id}>
+        <ExhibitionItem id={12} name="뿌슝" detail="뿌슝슝" imageList={[]} />
+      </div>
+    ))}
+    <ArtistInfo
+      img="https://images.unsplash.com/photo-1694900565922-d279cef76fd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3087&q=80"
+      part={part}
+      name={artistName}
+      info={artistInfo}
+    />
+  </styles.ExhibitionBackground>
+);
 export default Exhibition;
