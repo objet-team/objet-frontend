1ebddc37ec62a10a33c990df26233f20b737db04
JungminLee <85864699+JjungminLee@users.noreply.github.com>
2023-09-20 10:44:39 +0900

    feat:채용제의 모달 (#16)
    * feat:작품 인트로 페이지 퍼블리싱
    
    * refactor:로그인 모달  수정 + 네비바 디자인 맞춰 다시 수정
    
    * feat:전시조회 퍼블리싱
    
    * feat:채용제의 모달

---
+++ src/components/career/CareerModal.tsx
@@ -0,0 +1,63 @@
+import * as styles from './CarrerModal.style';
+import { COLORS } from '@/constants/styles';
+import Text from '../common/Text';
+import CloseButton from '@/assets/button/CloseButton.svg';
+import PurpleFillBtn from '../common/Button/PurpleFillBtn';
+
+export interface CareerModalProps {
+  careerClick: boolean;
+  setCareerClick: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const CareerModal = ({ careerClick, setCareerClick }: CareerModalProps) => {
+  const onClick = () => {
+    setCareerClick(!careerClick);
+  };
+  return (
+    <styles.ModalBackground>
+      <styles.ModalBox>
+        <div onClick={onClick}>
+          <CloseButton />
+        </div>
+        <Text color={COLORS.main.black} textStyleName="subtitle">
+          채용제안서를 위한 몇가지 정보를 입력해주세요
+        </Text>
+        <styles.ColContainer>
+          <styles.RowContainer>
+            <Text color={COLORS.main.black} textStyleName="body1">
+              회사명
+            </Text>
+            <Text color={COLORS.main.purple} textStyleName="body1">
+              *
+            </Text>
+          </styles.RowContainer>
+          <styles.InputContainer />
+        </styles.ColContainer>
+        <styles.ColContainer>
+          <styles.RowContainer>
+            <Text color={COLORS.main.black} textStyleName="body1">
+              내용
+            </Text>
+            <Text color={COLORS.main.purple} textStyleName="body1">
+              *
+            </Text>
+          </styles.RowContainer>
+          <styles.InputContainer />
+        </styles.ColContainer>
+        <styles.ColContainer>
+          <styles.RowContainer>
+            <Text color={COLORS.main.black} textStyleName="body1">
+              연락처
+            </Text>
+            <Text color={COLORS.main.purple} textStyleName="body1">
+              *
+            </Text>
+          </styles.RowContainer>
+          <styles.InputContainer />
+        </styles.ColContainer>
+        <PurpleFillBtn label="채용 제안하기" />
+      </styles.ModalBox>
+    </styles.ModalBackground>
+  );
+};
+export default CareerModal;
---
+++ src/components/common/Button/PurpleFillBtn/PurpleFillBtn.style..ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+import { COLORS } from '@/constants/styles';
+
+export const Button = styled.div`
+  width: 257px;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${COLORS.main.purple};
+  color: ${COLORS.main.white};
+`;
