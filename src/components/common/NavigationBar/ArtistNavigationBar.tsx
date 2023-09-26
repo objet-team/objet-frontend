b1e9d75f0a1f4f00790ed11d0f7d65570a7c837e
JjungminLee <julie0964@naver.com>
2023-09-26 11:48:25 +0900

    feat:Navigation bar user type에 맞춰 추가

---
+++ src/components/common/NavigationBar/ArtistNavigationBar.tsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import LogoIcon from '@public/icons/logo/LogoIcon.svg';
+import { COLORS } from '@/constants/styles';
+import Text from '@/components/common/Text';
+import * as styles from './NavigationBar.style';
+
+const ArtistNavigationBar = () => {
+  const [goodsClick, setGoodsClick] = useState(false);
+  const [uploadClick, setUploadClick] = useState(false);
+
+  const onGoodsClick = () => {
+    setGoodsClick(!goodsClick);
+  };
+  const onUploadClick = () => {
+    setUploadClick(!uploadClick);
+  };
+
+  return (
+    <div>
+      <styles.Wrapper>
+        <styles.NavMenu>
+          <Link href="/">
+            <LogoIcon />
+          </Link>
+
+          <Link href="/">
+            <Text color={COLORS.font.black100} textStyleName="body1">
+              홈
+            </Text>
+          </Link>
+          <Link href="/">
+            <Text color={COLORS.font.black100} textStyleName="body1">
+              굿즈샵
+            </Text>
+          </Link>
+        </styles.NavMenu>
+        <styles.ProfileMenu>
+          <styles.ProfileClickWrapper onClick={onGoodsClick}>
+            <Text color={COLORS.font.black100} textStyleName="body1">
+              작품업로드
+            </Text>
+          </styles.ProfileClickWrapper>
+          <styles.ProfileClickWrapper onClick={onUploadClick}>
+            <Text color={COLORS.font.black100} textStyleName="body1">
+              굿즈판매
+            </Text>
+          </styles.ProfileClickWrapper>
+        </styles.ProfileMenu>
+      </styles.Wrapper>
+    </div>
+  );
+};
+
+export default ArtistNavigationBar;
---
+++ src/components/common/NavigationBar/LoginAfterNavigationBar.tsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import LogoIcon from '@public/icons/logo/LogoIcon.svg';
+import { COLORS } from '@/constants/styles';
+import Text from '@/components/common/Text';
+import * as styles from './NavigationBar.style';
+import LoginModal from '@/components/login/LoginModal';
+import ArtistRegisterModal from '@/components/artistRegister/ArtistRegisterModal';
+
+const LoginAfterNavigationBar = () => {
+  const [loginClick, setLoginClick] = useState(false);
+  const [registerClick, setRegisterClick] = useState(false);
+  const [portalElement, setPortalElement] = useState<Element | null>(null);
+
+  useEffect(() => {
+    setPortalElement(document.getElementById('root-modal'));
+  }, [loginClick || registerClick]);
+  const onLoginClick = () => {
+    console.log(loginClick);
+    setLoginClick(!loginClick);
+  };
+  const onRegisterClick = () => {
+    setRegisterClick(!registerClick);
+  };
+
+  return (
+    <div>
+      <styles.Wrapper>
+        <styles.NavMenu>
+          <Link href="/">
+            <LogoIcon />
+          </Link>
+
+          <Link href="/">
+            <Text color={COLORS.font.black100} textStyleName="body1">
+              홈
+            </Text>
+          </Link>
+          <Link href="/">
+            <Text color={COLORS.font.black100} textStyleName="body1">
+              굿즈샵
+            </Text>
+          </Link>
+        </styles.NavMenu>
+        <styles.ProfileMenu>
+          <styles.ProfileImage />
+          {registerClick && portalElement ? (
+            <ArtistRegisterModal
+              setRegisterClick={setRegisterClick}
+              registerClick={registerClick}
+            />
+          ) : (
+            <styles.ProfileClickWrapper onClick={onRegisterClick}>
+              <Text color={COLORS.font.black100} textStyleName="body1">
+                작가 등록
+              </Text>
+            </styles.ProfileClickWrapper>
+          )}
+        </styles.ProfileMenu>
+      </styles.Wrapper>
+    </div>
+  );
+};
+
+export default LoginAfterNavigationBar;
