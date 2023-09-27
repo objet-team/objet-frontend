d6754b5dfffdaf258d819ef9acd60892b9a96294
JungminLee <85864699+JjungminLee@users.noreply.github.com>
2023-09-27 22:25:07 +0900

    fix:카카오로그인 (#44)
    * fix:배포 안되는 문제 해결(hooks type 문제)
    
    * fix:any 타입 전부 타입 지정
    
    * feat:홈화면 top rank api 연결
    
    * feat:최신순 api 연결
    
    * feat:카카오 로그인 api연결
    
    * fix:카카오 로그인 에러
    
    * fix:env file
    
    * fix:env
    
    * fix:카카오 로그인 에러

--- src/apis/postAuthLogin.ts
+++ src/apis/postAuthLogin.ts
@@ -5,12 +5,14 @@ export const postAuthLogin = async (codeParam: string): Promise<string> => {
   try {
     const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/auth/kakao`;
 
+    console.log('로그인진입');
     const data = await client.post(url, {
       authorizationCode: codeParam,
     });
     console.log(data);
-    return data.data;
+    return await data.data;
   } catch (error) {
+    console.log(error);
     throw error;
   }
 };
--- src/components/login/KakaoLoginRedirect.tsx
+++ src/components/login/KakaoLoginRedirect.tsx
@@ -3,25 +3,25 @@ import { useSearchParams } from 'next/navigation';
 import { useSetRecoilState } from 'recoil';
 import { useRouter } from 'next/router';
 import api from '@/services/TokenService';
-import { UserInfoAtomProps, userInfoAtom } from '@/states/UserInfoAtom';
 import getAuthLogin, { postAuthLogin } from '@/apis/postAuthLogin';
 
 const KakaoLoginRedirect = () => {
   const router = useRouter();
   const params = useSearchParams();
   const codeParam: string = params.get('code') as string;
-  console.log(codeParam);
+
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await postAuthLogin(codeParam);
-      console.log(response);
-      const token = response;
-      if (token) {
-        api.setToken(token);
-        router.push('/');
-      }
-    };
     if (codeParam != null) {
+      const fetchData = async () => {
+        console.log('리다이렉트');
+        const response = await postAuthLogin(codeParam);
+        console.log(response);
+        const token = response;
+        if (token) {
+          api.setToken(token);
+          router.push('/');
+        }
+      };
       fetchData();
     }
   }, [codeParam]);
