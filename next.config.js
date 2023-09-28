a39581a4c7cd8dd8e51c74ab251cedd81d914780
JjungminLee <julie0964@naver.com>
2023-09-28 19:46:17 +0900

    fix:s3 링크 추가

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
+    domains: ['images.unsplash.com', 'objet-bucket.s3.us-east-2.amazonaws.com'],
   },
   // async rewrites() {
   //   return [
