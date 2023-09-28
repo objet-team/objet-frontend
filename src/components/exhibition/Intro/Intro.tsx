2d5429772e1434f06ce98a5501f412d385b36a05
JjungminLee <julie0964@naver.com>
2023-09-28 20:34:25 +0900

    fix:deploy error

--- src/components/exhibition/Intro/Intro.tsx
+++ src/components/exhibition/Intro/Intro.tsx
@@ -30,7 +30,7 @@ const Intro = ({ title, description, artistInfo }: IntroProps) => {
       </div>
       <ArtistInfo
         img="https://images.unsplash.com/photo-1694572620159-e69049aeba00?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2970&q=80"
-        part="UXUI디자이너"
+        part={[]}
         info="나는 유어슈 미녀 로지"
         name="로지"
       />
