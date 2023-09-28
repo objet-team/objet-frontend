dd922487368e0866822bb84f67ce7cd9eadab67b
JjungminLee <julie0964@naver.com>
2023-09-28 20:55:32 +0900

    fix:deploy error

--- src/pages/exhibition/preview/content.tsx
+++ src/pages/exhibition/preview/content.tsx
@@ -16,7 +16,7 @@ const ExhibtionPreviewPage = () => {
         <Content data={data} />
         <ArtistInfo
           img="https://images.unsplash.com/photo-1694900565922-d279cef76fd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3087&q=80"
-          part="dfs"
+          part={[]}
           name="sdf"
           info="sdf"
         />
