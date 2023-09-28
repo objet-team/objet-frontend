c80d3edf93bb159c034f3faa8db97cfe812b035d
ballsona0925@naver.com <ballsona0925@naver.com>
2023-09-28 18:55:44 +0900

    fix: remove rewrite function

--- next.config.js
+++ next.config.js
@@ -5,14 +5,6 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'objet-bucket.s3.us-east-2.amazonaws.com'],
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/:path*',
-  //       destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
-  //     },
-  //   ];
-  // },
 
   webpack(config) {
     config.module.rules.push({
