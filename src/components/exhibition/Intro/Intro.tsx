90b1a0cb0f09536c8d0b0ae9123a5d141a136300
ballsona0925@naver.com <ballsona0925@naver.com>
2023-09-29 00:43:41 +0900

    fix: sovle rebase conflicts

--- src/components/exhibition/Intro/Intro.tsx
+++ src/components/exhibition/Intro/Intro.tsx
@@ -27,9 +27,7 @@ const Intro = ({ title, description, moveToExhibition }: IntroProps) => {
       >
         {description}
       </Text>
-      <div onClick={() => router.push(nextPath)}>
-        <DoorIcon />
-      </div>
+      <DoorIcon onClick={moveToExhibition} />
       <ArtistInfo
         img="https://images.unsplash.com/photo-1694572620159-e69049aeba00?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2970&q=80"
         part={[]}
