04d0fcb4e0d60826ade1dd833b6c83db520d6b38
ballsona0925@naver.com <ballsona0925@naver.com>
2023-10-04 15:21:21 +0900

    feat: modify client -> API

---
+++ src/apis/API.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import api from '@/services/TokenService';
+
+export const client = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    withCredentials: true,
+  },
+});
+
+client.interceptors.request.use((config) => {
+  if (!api.getToken() && !api.getId()) {
+    config.headers.authorization = `Bearer ${api.getToken()}`;
+  }
+
+  return config;
+});
+export default client;
