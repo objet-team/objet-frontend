b294254380b823234ed4364fd561a5d54863d9ed
ballsona0925@naver.com <ballsona0925@naver.com>
2023-10-14 15:09:37 +0900

    fix: edge to experimental-edge

--- src/pages/artistProfile/[id].tsx
+++ src/pages/artistProfile/[id].tsx
@@ -1,4 +1,4 @@
-export const runtime = 'edge';
+export const runtime = 'experimental-edge';
 
 import ArtistProfile from '@/components/ArtistProfile';
 import NavigationBar from '@/components/common/NavigationBar';
